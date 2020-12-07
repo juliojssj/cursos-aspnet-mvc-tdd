bd15fcb2fc9fe23265e62073cd947fe7fad44447
julio.sergio <juliojssj@hotmail.com>
2020-12-07 05:58:27 -0300

    first

---
+++ aspnet-mvc-tdd-master/MVC_TDD.Tests/Controllers/PessoaControllerTeste.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MVC_TDD.Controllers;
+using MVC_TDD.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace MVC_TDD.Tests.Controllers
+{
+    [TestClass]
+    public class PessoaControllerTeste
+    {
+        PessoaController controller = new PessoaController();
+
+        [TestMethod]
+        public void Index_Pessoa()
+        {
+            ViewResult result = controller.Index() as ViewResult;
+            Assert.IsInstanceOfType(result, typeof(ActionResult));
+        }
+
+        [TestMethod]
+        public void Create_Pessoa()
+        {
+            ViewResult result = controller.Create() as ViewResult;
+            Assert.IsTrue("Meu Create" == result.ViewBag.Titulo);
+        }
+
+        [TestMethod]
+        public void List_Pessoa()
+        {
+            ViewResult result = controller.List() as ViewResult;
+            var count = (result.Model as IEnumerable<Pessoa>).Count();
+            Assert.IsTrue(count > 0);
+        }
+    }
+}
---
+++ aspnet-mvc-tdd-master/MVC_TDD/Controllers/PessoaController.cs
@@ -0,0 +1,33 @@
+﻿using MVC_TDD.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_TDD.Controllers
+{
+    public class PessoaController : Controller
+    {
+        //
+        // GET: /Pessoa/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ViewResult Create()
+        {
+            ViewBag.Titulo = "Meu Create";
+            return View();
+        }
+
+        public ViewResult List()
+        {
+            List<Pessoa> lista = new List<Pessoa>();
+            lista.Add(new Pessoa("010101010101", "Joao"));
+
+            return View(lista);
+        }
+    }
+}
